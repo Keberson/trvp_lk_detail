47b4d59e202cc74b7c1773001d89ac74893ac57c
Keberson <creedkiller.62@gmail.com>
2024-01-04 23:17:28 +0300

    Добавил cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express, {Express} from 'express';
 import authRouter from './routes/auth.routes.js';
 import dashboardRouter from './routes/dashboard.routes.js';
+import cors from 'cors';
 
 const app: Express = express();
 const PORT: number = 5000;
 
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"]
+}));
+
 app.use('/api/login', authRouter);
 app.use('/api/dashboard', dashboardRouter);
 
